fc6b617457718c5d1e8ffb80fe162ac9e8bb82c6
Bonface Masira <bonfacemasira@gmail.com>
2022-06-15 15:41:00 +0300

    Done assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('form');
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+    const input = event.target['new-task-description'].value;
+    submitTask(input);
+    form.reset();
+  })
 });
+
+
+const submitTask = (todo) => {
+  const ul = document.querySelector("#tasks")
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.addEventListener("click", deleteTask)
+  delBtn.innerText = "X";
+  li.textContent =`${todo} `;
+  li.appendChild(delBtn)
+  ul.appendChild(li);
+  
+}
+
+const deleteTask = (event) => {
+   event.target.parentNode.remove();
+}
